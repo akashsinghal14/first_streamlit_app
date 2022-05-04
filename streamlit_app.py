af2778db8b214560b55882407023f18ed035e371
akashsinghal14 <akashsinghal14@yahoo.com>
2022-05-04 14:37:31 +0530

    Update streamlit_app.py
    new lesson

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('Haha its your first title, lets see')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
