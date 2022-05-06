7fc65bf7016cf6939fb5d576c6348f852815300b
akashsinghal14 <akashsinghal14@yahoo.com>
2022-05-06 19:42:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('what fruit information?')
-  if not fruit_choice
+  if not fruit_choice:
       streamlit.error("Please select a fruit to get info.")
   else:
       fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
